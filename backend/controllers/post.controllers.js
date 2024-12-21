6813ddd3742d6cb756e6d784600aa5186eb3638a
Damian Oguche <damianguch@yahoo.com>
2024-12-21 05:32:27 +0100

    Create post functionality added

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -109,7 +109,6 @@ export const createPost = async (req, res) => {
     const userId = req.user._id.toString();
 
     const user = await User.findById(userId);
-
     if (!user)
       return res.status(404).json({
         success: false,
