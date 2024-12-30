9d6d57462802c5c3609f1d6e176879d3ada5ca02
Damian Oguche <damianguch@yahoo.com>
2024-12-30 16:41:15 +0100

    modified backend/controllers/post.controllers.js  to return posts

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -57,10 +57,13 @@ export const getLikedPosts = async (req, res) => {
 export const getFollowingPosts = async (req, res) => {
   try {
     const userId = req.user._id;
-
     const user = await User.findById(userId);
+
     if (!user) {
-      return res.status(404).json({ success: false, error: 'User not found!' });
+      return res.status(404).json({
+        success: false,
+        error: 'User not found!'
+      });
     }
 
     const following = user.following;
@@ -70,7 +73,10 @@ export const getFollowingPosts = async (req, res) => {
       .populate({ path: 'user', select: '-password' })
       .populate({ path: 'comments.user', select: '-password' });
 
-    res.status(200).json({ success: true, feedPosts });
+    res.status(200).json({
+      success: true,
+      posts
+    });
   } catch (err) {
     logger.error(`Error in getFollowingPosts controller: ${err.message}`);
     res.status(500).json({
