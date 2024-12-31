fecbf79830015824eceb1e4b67b4fe6d8b46bf45
Damian Oguche <damianguch@yahoo.com>
2024-12-31 15:08:21 +0100

    Added the dist folder to the content array

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,11 @@ import daisyUIThemes, { black } from 'daisyui/src/theming/themes';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './dist/**/*.{html,js}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {}
   },
