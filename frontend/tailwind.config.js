bbc7ada1d640f21c468e9bbf98bcceddf203472e
Damian Oguche <damianguch@yahoo.com>
2024-12-31 05:28:10 +0100

    Modified Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,10 @@ import daisyUIThemes, { black } from 'daisyui/src/theming/themes';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./pages/**/*{js,ts,jsx,tsx}', './components/**/*{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {}
   },
