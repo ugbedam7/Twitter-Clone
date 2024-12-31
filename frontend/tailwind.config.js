9e85ba758f984d034f890aa25d578f329272852e
Damian Oguche <damianguch@yahoo.com>
2024-12-31 14:41:52 +0100

    Set the purge array to empy

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,6 @@ import daisyUIThemes, { black } from 'daisyui/src/theming/themes';
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: [],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
